c04c768eb98d3c2934302ccbd86abcccb52f5a21
malanak2 <malanakgames@seznam.cz>
2024-02-07 19:38:56 +0100

    Works, does nothing with the data yet

--- src/commands/fetch_api.js
+++ src/commands/fetch_api.js
@@ -1,25 +1,78 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js')
+const puppeteer = require('puppeteer')
 
 const api_link = "https://domjudge.zapotocnylubos.com";
-
+require('dotenv').config()
 module.exports = {
     register_command: new SlashCommandBuilder()
         .setName('fetch_api')
         .setDescription('Fetches data from API!'),
     async execute(client, interaction) {
-        let response = await fetch(api_link + "/api/v4/?strict=false&onlyActive=true", {
-            method: "GET",
-            headers: {},
-            /*body: JSON.stringify({
-                ids: [],
-                strict: false,
-                onlyActive: true
-            })*/
+        const reply = await interaction.reply("‎");
+        const testEmbed = new Discord.EmbedBuilder().setColor(0x0099FF).setTitle("Scraping in progress...");
+        reply.edit({ embeds: [testEmbed], components: [] });
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
         });
-        let data= response.json();
-        if (response.status !== 200)return interaction.reply({content:"Failed to fetch data from API. HTTP Code: " + response.status,ephemeral :true});
-        console.log(response.status);
-        return interaction.reply({content:JSON.stringify(data),ephemeral :true});
+
+        const page = await browser.newPage();
+        await page.setViewport({width: 1200, height: 720})
+        await page.goto(api_link + "/login", {
+            waitUntil: "networkidle0",
+        });
+
+        // await page.waitForNavigation();
+
+        await page.type('#username', "YOUR USERNAME HERE");
+        await page.type('#inputPassword', "YOUR_PASSWORD_HERE");
+
+        await page.click('button[type="submit"]')
+
+        await page.goto(api_link + "/team/scoreboard", {
+            waitUntil: "networkidle0",
+        });
+
+        let name = await page.evaluate(()=>{
+            let rawname =document.querySelector(".card-header").innerHTML.replace('<span style="font-weight: bold;">', "");
+            return rawname.slice(0, rawname.length - 215 - 7 - 2);
+        })
+
+        let data = await page.evaluate(() => {
+            return Array.from(
+                document.querySelectorAll('table[class="scoreboard center "] > tbody > tr'),
+                row => Array.from(row.querySelectorAll('th, td'), cell => cell.innerText)
+            )
+            /*const rawname = document.querySelector(".card-header").innerHTML.replace('<span style="font-weight: bold;">', "");
+            /*const contestantsarr = (() => {
+
+            });
+            const table = document.querySelector(".scoreboard")
+            const tbody = table.childNodes[4];
+            const content = tbody.childNodes;
+            let arr = "";
+            for (let i = 0; i < content.length - 1; i++) {
+                arr += `|Rank: ${content[i].childNodes[0].nodeName}, Name: ${content[i].childNodes[2].nodeName}, Completed úlohy: , Skóre:`;
+                //console.log(`Rank: ${content[i].childNodes[0].textContent}, Name: ${content[i].childNodes[2].attr("title")}, Completed úlohy: , Skóre:`);
+            }
+            return arr + "return val";//rawname.slice(0, rawname.length - 215 - 7 - 2);
+            //return rawname.innerHTML;*/
+        })
+        const repEmbed = new Discord.EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(name)
+            //.addFields([name="Name", value=data])
+        ;
+        for(let i = 0; i < data.length-1; i++)
+        {
+            repEmbed.addFields({name: data[i][2], value: `Rank: ${data[i][0]}, Finished Trials: ${data[i][3]}, Total Score: ${data[i][4]}`});
+        }
+
+
+        console.log("");
+        await browser.close();
+
+        return reply.edit({embeds:[repEmbed],ephemeral :true});
     }
 }
